cda28a986a1454f7059a5c966256e6f19889e1ca
akeyi2018 <akeyi2016@gmail.com>
2023-01-22 09:05:01 +0900

    add main.py

--- main.py
+++ main.py
@@ -1,1 +1,23 @@
-import pandas as pd
+from flask import Flask, request, render_template
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+
+    return render_template('main.html')
+
+@app.route('/regist')
+def regist():
+
+    return render_template('regist.html')
+
+
+@app.route('/view')
+def view():
+    return render_template('view.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True, host='0.0.0.0')
+
+
